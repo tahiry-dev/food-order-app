3adfb0313403c8cc721980741250f5621df4f065
tahiry-dev <tahirycontact@gmail.com>
2022-12-21 15:33:06 +0300

    feat: manage components with state and prop

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,7 +7,7 @@ const Header = (props) => {
     <>
       <header className={classes.header}>
         <h1>Order Your Meals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton headerClicked = {props.onShowCart}/>
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt='A table full of delicious food!' />
